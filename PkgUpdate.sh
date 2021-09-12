6e586c7ba5b0ef5ac15df1cb706d5e6a189a3619
BondnetwordS1 <88929961+BondnetwordS1@users.noreply.github.com>
2021-09-12 13:44:13 +0700

    Add files via upload

---
+++ PkgUpdate.sh
@@ -0,0 +1,23 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m' 
+cyan='\033[36;1m' 
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m' 
+sleep 1
+echo
+toilet -f big -F gay Update
+echo
+echo $blue"script ini untuk download file buat download script lain"
+
+pkg update && pkg upgrade
+pkg install git
+pkg install python
+pkg install python2
+pkg install toilet
+pkg install ruby
+pkg install echo
+pkg install figlet
+pkg install bash
